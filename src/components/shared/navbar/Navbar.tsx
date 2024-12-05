7e385777ae75f52387a111ae37eab197d398f018
NafizUddin <nafizuddin.ctg@gmail.com>
2024-12-05 20:17:43 +0600

    added cart drawer

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -14,52 +14,71 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
 import { siteConfig } from "@/src/config/site";
-import { Button } from "@nextui-org/button";
 import logo from "@/src/assets/logo.png";
 import useUserDetails from "@/src/hooks/CustomHooks/useUserDetails";
 import NavbarUserDropdown from "./NavbarUserDropdown";
+import { IoSearch } from "react-icons/io5";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import CartDrawer from "../../CartDrawer/CartDrawer";
 
 export default function Navbar() {
   const pathname = usePathname();
   const { userData, isLoading } = useUserDetails();
 
   return (
-    <NextUINavbar
-      maxWidth="xl"
-      position="sticky"
-      className="pt-2 pb-1 lg:pb-4"
-      classNames={{
-        item: [
-          "flex",
-          "relative",
-          "h-full",
-          "items-center",
-          "data-[active=true]:after:content-['']",
-          "data-[active=true]:after:absolute",
-          "data-[active=true]:after:-bottom-3",
-          "data-[active=true]:after:left-0",
-          "data-[active=true]:after:right-0",
-          "data-[active=true]:after:h-[3px]",
-          "data-[active=true]:after:rounded-[3px]",
-          "data-[active=true]:after:bg-primary",
-        ],
-      }}
-    >
-      <NavbarBrand className="mt-3">
-        <div className="lg:hidden">
-          <NavbarMenuToggle />
-        </div>
-        <Image
+    <>
+      <NextUINavbar
+        maxWidth="xl"
+        position="sticky"
+        className="pt-2 pb-1 lg:pb-4"
+        classNames={{
+          item: [
+            "flex",
+            "relative",
+            "h-full",
+            "items-center",
+            "data-[active=true]:after:content-['']",
+            "data-[active=true]:after:absolute",
+            "data-[active=true]:after:-bottom-3",
+            "data-[active=true]:after:left-0",
+            "data-[active=true]:after:right-0",
+            "data-[active=true]:after:h-[3px]",
+            "data-[active=true]:after:rounded-[3px]",
+            "data-[active=true]:after:bg-primary",
+          ],
+        }}
+      >
+        <NavbarBrand className="mt-3">
+          <div className="lg:hidden">
+            <NavbarMenuToggle />
+          </div>
+          {/* <Image
           src={logo}
           alt="logo"
           height={80}
           width={80}
           className="hidden lg:flex py-1"
-        />
-      </NavbarBrand>
+        /> */}
+          <div className="hidden lg:flex gap-6">
+            {siteConfig.navItems.map((item) => (
+              <NavbarItem
+                className={`text-lg ${pathname === item.href ? "text-primary" : "text-white"}`}
+                key={item.label}
+                isActive={pathname === item.href}
+              >
+                <Link
+                  href={item.href}
+                  aria-current={pathname === item.href ? "page" : undefined}
+                >
+                  {item.label}
+                </Link>
+              </NavbarItem>
+            ))}
+          </div>
+        </NavbarBrand>
 
-      <NavbarContent justify="center">
-        <div className="hidden lg:flex gap-6">
+        <NavbarContent justify="center">
+          {/* <div className="hidden lg:flex gap-6">
           {siteConfig.navItems.map((item) => (
             <NavbarItem
               className={`text-lg ${pathname === item.href ? "text-primary" : "text-white"}`}
@@ -74,48 +93,86 @@ export default function Navbar() {
               </Link>
             </NavbarItem>
           ))}
-        </div>
-        {/* <Image
-          src={logo}
-          alt="logo"
-          height={170}
-          width={170}
-          className="flex lg:hidden"
-        /> */}
-      </NavbarContent>
+        </div> */}
+          <Image
+            src={logo}
+            alt="logo"
+            height={80}
+            width={80}
+            className="hidden lg:flex py-1 mt-2"
+          />
+          <Image
+            src={logo}
+            alt="logo"
+            height={80}
+            width={70}
+            className="flex lg:hidden mt-5"
+          />
+        </NavbarContent>
 
-      <NavbarContent justify="end">
-        <NavbarItem className="flex">
-          {isLoading ? (
-            <div className="animate-pulse w-10 h-10 rounded-full bg-gray-400" />
-          ) : userData ? (
-            <NavbarUserDropdown user={userData} />
-          ) : (
-            <Link href="/login">
-              <div className="hidden md:block">
-                <Button color="primary">Login</Button>
+        <NavbarContent justify="end">
+          <NavbarItem className="flex gap-4 items-center">
+            <IoSearch className="text-white text-2xl cursor-pointer" />
+            {/* Cart Button */}
+            {/* <MdOutlineShoppingCart
+              onClick={toggleDrawer}
+              className="text-white text-2xl cursor-pointer"
+            /> */}
+
+            <div className="drawer-end">
+              <input
+                id="my-drawer-4"
+                type="checkbox"
+                className="drawer-toggle"
+              />
+              <div className="drawer-content mr-3">
+                {/* Page content here */}
+                <label htmlFor="my-drawer-4" className="drawer-button">
+                  <MdOutlineShoppingCart className="text-white text-2xl cursor-pointer" />
+                </label>
               </div>
-            </Link>
-          )}
-        </NavbarItem>
-      </NavbarContent>
+              <div className="drawer-side z-20">
+                <label
+                  htmlFor="my-drawer-4"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                />
+                <CartDrawer />
+              </div>
+            </div>
+            {isLoading ? (
+              <div className="animate-pulse w-10 h-10 rounded-full bg-gray-400" />
+            ) : userData ? (
+              <NavbarUserDropdown user={userData} />
+            ) : (
+              <Link href="/login">
+                <div className="hidden md:block">
+                  <button className="relative h-10 w-30 origin-top transform rounded-lg border-2 border-primary text-primary before:absolute before:top-0 before:block before:h-0 before:w-full before:duration-500 hover:text-white hover:before:absolute hover:before:left-0 hover:before:-z-10 hover:before:h-full hover:before:bg-primary uppercase font-bold px-3">
+                    Login
+                  </button>
+                </div>
+              </Link>
+            )}
+          </NavbarItem>
+        </NavbarContent>
 
-      <NavbarMenu>
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item.label}-${index}`}>
-              <NextUILink
-                color="primary"
-                href={item.href}
-                size="lg"
-                className="font-bold"
-              >
-                {item.label}
-              </NextUILink>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
-    </NextUINavbar>
+        <NavbarMenu>
+          <div className="mx-4 mt-2 flex flex-col gap-2">
+            {siteConfig.navMenuItems.map((item, index) => (
+              <NavbarMenuItem key={`${item.label}-${index}`}>
+                <NextUILink
+                  color="primary"
+                  href={item.href}
+                  size="lg"
+                  className="font-bold"
+                >
+                  {item.label}
+                </NextUILink>
+              </NavbarMenuItem>
+            ))}
+          </div>
+        </NavbarMenu>
+      </NextUINavbar>
+    </>
   );
 }
