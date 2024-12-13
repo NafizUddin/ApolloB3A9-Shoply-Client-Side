f669f8e7c51487bd5485f117bf8c7e0e73f4788a
NafizUddin <nafizuddin.ctg@gmail.com>
2024-12-14 05:32:35 +0600

    added table loading skeleton

--- src/app/(DashboardLayout)/(customerDashboard)/customer-dashboard/myOrders/page.tsx
+++ src/app/(DashboardLayout)/(customerDashboard)/customer-dashboard/myOrders/page.tsx
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { IOrder } from "@/src/types/model";
 import { Pagination } from "@nextui-org/pagination";
+import TableLoadingSkeleton from "@/src/components/LoadingCards/TableLoading";
 
 const MyOrders = () => {
   const { userData } = useUserDetails();
@@ -18,7 +19,9 @@ const MyOrders = () => {
     customerId: userData?.userData?.id,
   });
 
-  const { data: customerOrders, isLoading } = useGetAllOrdersQuery(queryObj);
+  const { data: customerOrders, isLoading } = useGetAllOrdersQuery(queryObj, {
+    skip: !userData?.userData,
+  });
 
   const totalPages = Math.ceil(
     (customerOrders?.meta?.total || 0) / dataPerPage
@@ -27,8 +30,6 @@ const MyOrders = () => {
   const handlePageChange = (page: number) => {
     setCurrentPage(page);
   };
-
-  console.log(totalPages);
 
   useEffect(() => {
     setQueryObj((prev) => ({
@@ -39,84 +40,93 @@ const MyOrders = () => {
     }));
   }, [currentPage, userData?.userData]);
 
-  console.log(customerOrders?.data[0]?.orderDetails[0]?.product?.image[0]);
-
   return (
     <div>
       <DashboardSectionTitle heading="My Orders" />
 
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.6, ease: "easeOut", delay: 0.9 }}
-        style={{
-          scrollbarWidth: "none" /* Firefox */,
-          msOverflowStyle: "none" /* IE and Edge */,
-          overflowY: "auto" /* Enable scrolling */,
-        }}
-        className="overflow-x-auto m-5"
-      >
-        <table className="table">
-          {/* head */}
-          <thead className="text-lg">
-            <tr>
-              <th className="text-gray-300">No.</th>
-              <th className="text-gray-300">Product Image</th>
-              <th className="text-gray-300">Quantity</th>
-              <th className="text-gray-300">Shop Name</th>
-              <th className="text-gray-300">Total Price</th>
-              <th className="text-gray-300">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {customerOrders?.data.length > 0 &&
-              customerOrders?.data?.map(
-                (singleOrder: IOrder, index: number) => {
-                  return (
-                    <tr key={index} className="rounded-lg">
-                      <th className="text-lg text-white">
-                        {index + 1 + (currentPage - 1) * dataPerPage}
-                      </th>
-                      <td className="flex justify-center items-center">
-                        <img
-                          src={singleOrder?.orderDetails[0]?.product?.image[0]}
-                          alt="product"
-                          className="w-14 h-14 rounded-xl object-cover"
-                        />
-                      </td>
-                      <td className="text-lg font-semibold text-center text-white">
-                        {singleOrder?.orderDetails[0]?.quantity}
-                      </td>
-                      <td className="text-lg font-semibold text-white">
-                        {singleOrder?.vendor?.shopName}
-                      </td>
-                      <td className="font-semibold text-lg text-white">
-                        <span>$</span>
-                        {(singleOrder?.totalPrice).toFixed(2)}
-                      </td>
-                      <td className="">
-                        <button className="relative h-10 w-30 origin-top transform rounded-lg border-2 border-primary text-primary before:absolute before:top-0 before:block before:h-0 before:w-full before:duration-500 hover:text-white hover:before:absolute hover:before:left-0 hover:before:-z-10 hover:before:h-full hover:before:bg-primary uppercase font-bold px-3">
-                          Add Review
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                }
-              )}
-          </tbody>
-        </table>
-      </motion.div>
+      <div>
+        {isLoading ? (
+          <TableLoadingSkeleton />
+        ) : (
+          <div>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.6, ease: "easeOut", delay: 0.9 }}
+              style={{
+                scrollbarWidth: "none" /* Firefox */,
+                msOverflowStyle: "none" /* IE and Edge */,
+                overflowY: "auto" /* Enable scrolling */,
+              }}
+              className="overflow-x-auto mt-8"
+            >
+              <table className="table">
+                {/* head */}
+                <thead className="text-lg">
+                  <tr>
+                    <th className="text-gray-300">No.</th>
+                    <th className="text-gray-300">Product Image</th>
+                    <th className="text-gray-300">Quantity</th>
+                    <th className="text-gray-300">Shop Name</th>
+                    <th className="text-gray-300">Total Price</th>
+                    <th className="text-gray-300">Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {customerOrders?.data.length > 0 &&
+                    customerOrders?.data?.map(
+                      (singleOrder: IOrder, index: number) => {
+                        return (
+                          <tr key={index} className="rounded-lg">
+                            <th className="text-lg text-white">
+                              {index + 1 + (currentPage - 1) * dataPerPage}
+                            </th>
+                            <td className="flex justify-center items-center">
+                              <img
+                                src={
+                                  singleOrder?.orderDetails[0]?.product
+                                    ?.image[0]
+                                }
+                                alt="product"
+                                className="w-14 h-14 rounded-xl object-cover"
+                              />
+                            </td>
+                            <td className="text-lg font-semibold text-center text-white">
+                              {singleOrder?.orderDetails[0]?.quantity}
+                            </td>
+                            <td className="text-lg font-semibold text-white">
+                              {singleOrder?.vendor?.shopName}
+                            </td>
+                            <td className="font-semibold text-lg text-white">
+                              <span>$</span>
+                              {(singleOrder?.totalPrice).toFixed(2)}
+                            </td>
+                            <td className="">
+                              <button className="relative h-10 w-30 origin-top transform rounded-lg border-2 border-primary text-primary before:absolute before:top-0 before:block before:h-0 before:w-full before:duration-500 hover:text-white hover:before:absolute hover:before:left-0 hover:before:-z-10 hover:before:h-full hover:before:bg-primary uppercase font-bold px-3">
+                                Add Review
+                              </button>
+                            </td>
+                          </tr>
+                        );
+                      }
+                    )}
+                </tbody>
+              </table>
+            </motion.div>
 
-      <div className="pt-7">
-        {customerOrders?.data?.length > 0 && (
-          <div className="flex justify-center items-center mt-4">
-            <Pagination
-              total={totalPages}
-              initialPage={1}
-              page={currentPage}
-              onChange={handlePageChange}
-              showControls
-            />
+            <div className="pt-7">
+              {customerOrders?.data?.length > 0 && (
+                <div className="flex justify-center items-center mt-4">
+                  <Pagination
+                    total={totalPages}
+                    initialPage={1}
+                    page={currentPage}
+                    onChange={handlePageChange}
+                    showControls
+                  />
+                </div>
+              )}
+            </div>
           </div>
         )}
       </div>
