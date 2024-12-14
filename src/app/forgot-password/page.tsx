b8bc7bc757b9db9c53b44e506f9ed897001e83c7
NafizUddin <nafizuddin.ctg@gmail.com>
2024-12-15 03:19:01 +0600

    added forgot & reset password service

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -1,16 +1,17 @@
+/* eslint-disable react/self-closing-comp */
 "use client";
 import logo from "@/src/assets/logo.png";
-import { forgotPassword } from "@/src/services/AuthServices";
+import { forgotPassword } from "@/src/utils/loginService";
 import { Input } from "@nextui-org/input";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import { useMemo, useState } from "react";
 import toast from "react-hot-toast";
-import { ImSpinner6 } from "react-icons/im";
 
 const ForgotPassword = () => {
   const [value, setValue] = useState("");
-  const [loading, setLoading] = useState(false);
+  const router = useRouter();
 
   const validateEmail = (value: string) =>
     value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
@@ -23,28 +24,30 @@ const ForgotPassword = () => {
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    setLoading(true);
     toast.loading("Loading...");
 
     const userData = { email: value };
 
-    const response = await forgotPassword(userData);
-    toast.dismiss();
+    try {
+      const response = await forgotPassword(userData);
+      toast.dismiss();
 
-    if (response?.success) {
-      setLoading(false);
-      setValue("");
-      return toast.success("Password reset URL has been sent to your email", {
-        duration: 6000,
-      });
+      if ("success" in response && response?.success) {
+        setValue("");
+        router.push("/login");
+        return toast.success("Please check email inbox or spam!", {
+          duration: 6000,
+        });
+      }
+    } catch (error: any) {
+      toast.error(error.message);
     }
   };
 
   return (
-    <section className="bg-gray-50">
+    <section>
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-        <div className="w-full p-6 bg-white rounded-lg shadow md:mt-0 sm:max-w-md sm:p-8">
+        <div className="w-full p-6 bg-[#18181B] rounded-lg shadow md:mt-0 sm:max-w-md sm:p-8">
           <div className="flex justify-center items-center">
             <Link
               href={"/"}
@@ -54,16 +57,16 @@ const ForgotPassword = () => {
                 className=""
                 src={logo}
                 alt="logo"
-                width={200}
-                height={200}
+                width={80}
+                height={80}
               />
             </Link>
           </div>
 
-          <h1 className="mb-1 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+          <h1 className="mb-1 text-xl font-bold leading-tight tracking-tight text-white md:text-2xl">
             Forgot your password?
           </h1>
-          <p className="font-light text-gray-500">
+          <p className="font-light text-gray-400">
             Don&apos;t fret! Just type in your email and we will send you a URL
             to reset your password!
           </p>
@@ -78,7 +81,7 @@ const ForgotPassword = () => {
                 placeholder="Enter your email..."
                 variant="bordered"
                 isInvalid={isInvalid}
-                color={isInvalid ? "danger" : "success"}
+                color={isInvalid ? "danger" : "primary"}
                 errorMessage="Please enter a valid email"
                 onValueChange={setValue}
                 size="lg"
@@ -86,19 +89,9 @@ const ForgotPassword = () => {
             </div>
             <button
               type="submit"
-              className="group relative z-10 h-11 w-full overflow-hidden bg-primary text-white rounded-full text-center font-semibold text-lg"
+              className="relative h-12 w-30 origin-top transform rounded-lg border-2 border-primary text-primary before:absolute before:top-0 before:block before:h-0 before:w-full before:duration-500 hover:text-white hover:before:absolute hover:before:left-0 hover:before:-z-10 hover:before:h-full hover:before:bg-primary uppercase font-bold px-3"
             >
-              <span className="absolute -inset-24 origin-left rotate-12 scale-x-0 transform bg-white transition-transform duration-700 group-hover:scale-x-100 group-hover:duration-300"></span>
-              <span className="absolute -inset-24 origin-left rotate-12 scale-x-0 transform bg-sky-700 transition-transform duration-500 group-hover:scale-x-100 group-hover:duration-700"></span>
-              <span className="absolute -inset-24 origin-left rotate-12 scale-x-0 transform bg-sky-900 transition-transform duration-300 group-hover:scale-x-50 group-hover:duration-500"></span>
-              <span className="absolute z-10 text-center text-white opacity-0 duration-100 ease-out group-hover:opacity-100 group-hover:duration-700">
-                {loading ? "" : "Reset Password"}
-              </span>
-              {loading ? (
-                <ImSpinner6 className="animate-spin m-auto text-xl" />
-              ) : (
-                "Reset Password"
-              )}
+              Reset Password
             </button>
           </form>
         </div>
--- src/utils/loginService.ts
+++ src/utils/loginService.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { cookies } from "next/headers";
+import envConfig from "../config/envConfig";
 
 export const loginUser = async (userData: Record<string, any>) => {
   try {
@@ -99,4 +100,61 @@ export const getAccessToken = async () => {
   const accessToken = cookies().get("accessToken")?.value;
 
   return accessToken;
+};
+
+export const forgotPassword = async (userEmail: { email: string }) => {
+  console.log(userEmail);
+  try {
+    const response = await fetch(`${envConfig.baseApi}/auth/forgot-password`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userEmail),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      throw new Error(errorData.message || "Failed to send reset link");
+    }
+
+    const result = await response.json();
+    console.log("Response received:", result);
+    return result;
+  } catch (error: any) {
+    console.error("Error in forgotPassword:", error);
+    throw error;
+  }
+};
+
+export const resetPassword = async (
+  userData: {
+    email: string;
+    newPassword: string;
+  },
+  token: string
+) => {
+  try {
+    const response = await fetch(`${envConfig.baseApi}/auth/reset-password`, {
+      method: "POST",
+      headers: {
+        Authorization: `${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
+    });
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Error response:", errorData);
+      throw new Error(errorData.message || "Failed to reset password");
+    }
+
+    const result = await response.json();
+    console.log("Response received:", result);
+    return result;
+  } catch (error: any) {
+    console.error("Error in resetPassword:", error);
+    throw error;
+  }
 };
